2b1b4ca472d5328cb08add10b87737faef04c09c
monirul360 <monirulislam65361@gmail.com>
2022-09-26 19:43:16 +0600

    contact page complate

--- src/Page/Contact/Contact.js
+++ src/Page/Contact/Contact.js
@@ -6,16 +6,18 @@ const Contact = () => {
             <section className='contact p-5'>
                 <div className="row d-flex justify-content-center align-items-center">
                     <div className="col-md-6">
-                        <div className='contact-text m-3'>
+                        <div className='contact-text  m-4'>
                             <h1>
                                 Get in touch
                             </h1>
-                            <p>I'm available for new projects and collaborations.</p>
-                            <p>Don't hesitate to reach out!</p>
+                            <p>I'm available for new projects and collaborations.
+                                Don't hesitate to reach out!
+                            </p>
+
                         </div>
                     </div>
                     <div className="col-md-6">
-                        <div className="contact-from p-3">
+                        <div className="contact-from m-4">
                             <Form></Form>
                         </div>
                     </div>
