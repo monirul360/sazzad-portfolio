d9791a873ba7aa8bb5b07455bbfe8c4fc7ddb2ca
monirul360 <monirulislam65361@gmail.com>
2022-10-03 00:04:34 +0600

    all problme solv

--- src/Page/works/Infowork.js
+++ src/Page/works/Infowork.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Nav from '../Share/Nav/Nav';
 const Infowork = () => {
     const { id } = useParams();
     const [items, setItems] = useState({});
@@ -11,6 +12,7 @@ const Infowork = () => {
     }, [id]);
     return (
         <div>
+            <Nav></Nav>
             <div className="container">
                 <div className="infowork">
                     <h2>{items?.Title}</h2>
@@ -52,7 +54,7 @@ const Infowork = () => {
 
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
